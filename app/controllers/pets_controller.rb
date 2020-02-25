5d3e6aa6647cde8d812e76c64b81a97aa7ff0bc5
svenia wurm <svwu@outlook.com>
2020-02-25 14:49:40 +0100

    added a new route and controller actions new and create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,4 +4,20 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_param)
+    @pet.save
+    redirect_to pets_path
+  end
+
+  private
+
+  def pet_param
+    params.require(:pet).permit(:name, :price_in_eur, :description, :pet_type)
+  end
 end
