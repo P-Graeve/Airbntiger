cc57721e07ed0f1a8cc8f2cc3d72af8a002f5756
Matthijs Kralt <matthijs29@live.com>
2020-02-25 11:28:47 +0100

    Rails generate Booking model

---
+++ db/migrate/20200225102818_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.references :user, foreign_key: true
+      t.references :pet, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
