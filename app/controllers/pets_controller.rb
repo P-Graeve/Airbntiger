a0d45c08f438d157f24fbd3009557be314f11303
Matthijs Kralt <matthijs29@live.com>
2020-02-25 14:51:20 +0100

    Add params filtering query in pets#index

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,6 +2,11 @@ class PetsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @pets = Pet.all
+    # check if params are present
+    if params.present?
+      @pets = Pets.where("name ILIKE ?", params[:query])
+    else
+      @pets = Pet.all
+    end
   end
 end
