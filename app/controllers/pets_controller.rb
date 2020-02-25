b367d708661a95cf868bffff36caad1731380a19
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-02-25 15:28:05 +0100

    added show to the controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,12 @@
 class PetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @pets = Pet.all
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
 end
