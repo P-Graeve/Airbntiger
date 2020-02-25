9a7bd3972cc4ca9aa30d499c7ee9c921ff8ebd46
Matthijs Kralt <matthijs29@live.com>
2020-02-25 10:28:27 +0100

    Add TYPES constant to Pet model, add validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,13 @@
 class Pet < ApplicationRecord
+  # constant TYPES has array of all allowed types of pets
+  TYPES = %w(dog hamster cat tiger lion rhino fish turtle iguana rat parrot)
+
+  # validate if name is present
+  validates :name, presence: true
+  # validate if price is present and is >= 0
+  validates :price_in_eur, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  # validate if the description is there and has more than 140 characters
+  validates :description, presence: true, length: { minimum: 140 }
+  # validate if the type of part of Pet::TYPES
+  validates :type, presence: true, inclusion: { in: TYPES }
 end
