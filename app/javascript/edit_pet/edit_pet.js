57555cd9d3e0acbc485be0192814baad32cff365
Matthijs Kralt <matthijs29@live.com>
2020-02-26 15:50:51 +0100

    Remove photos field in edit

---
+++ app/javascript/edit_pet/edit_pet.js
@@ -0,0 +1,7 @@
+export const refreshImages = () => {
+  // just hide this thing for now
+  document.querySelector('')
+  // get all images from the dataset
+  const data = document.querySelector("#image-container").dataset;
+  console.log(data.photos);
+}
