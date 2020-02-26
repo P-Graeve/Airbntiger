b3d763c12549d7126a2955c7253d71b789471ba4
Matthijs Kralt <matthijs29@live.com>
2020-02-26 11:54:17 +0100

    Add change listener to input that is hidden

--- app/javascript/signup/sign_up.js
+++ app/javascript/signup/sign_up.js
@@ -1,4 +1,14 @@
 export const initAvatarUploadBtn = () => {
+  const input = document.querySelector("#user_avatar");
   const label = document.querySelector("label.file");
-  console.log(label);
+  // set innertext to 'upload avatar' with icon
+  label.innerHTML = `
+    <i class="fas fa-upload"></i>
+    <span>Upload Avatar</span>
+  `;
+
+  // add an event listener to the input, for when an avatar is uploaded, change the label
+  input.addEventListener('change', e => {
+    displayAvatar(e, label);
+  });
 }
