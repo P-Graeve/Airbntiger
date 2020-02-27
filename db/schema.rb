b2077c026fef2a90138367c8a68b532f95cd6c78
Matthijs Kralt <matthijs29@live.com>
2020-02-27 13:55:51 +0100

    Add migration: Create Geolocations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_151648) do
+ActiveRecord::Schema.define(version: 2020_02_27_125020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2020_02_25_151648) do
     t.datetime "updated_at", null: false
     t.index ["pet_id"], name: "index_bookings_on_pet_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "geolocations", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longtitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pets", force: :cascade do |t|
