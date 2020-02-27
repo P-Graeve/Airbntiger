fa6e00e0b5854a2e84d35ad8ee62bd503597025a
Matthijs Kralt <matthijs29@live.com>
2020-02-27 14:09:57 +0100

    Make image loading optional for the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
+
+loadImages = false
 # remove all pets
 puts "Cleaning DB..."
 Booking.destroy_all
@@ -13,33 +15,40 @@ Pet.destroy_all
 User.destroy_all
 # create 4 pets
 stinky = Pet.new(name: 'Stinky', price_in_eur: 15, pet_type: 'Tiger', description: 'Stinky is a great tiger')
-tiger_img = URI.open('https://i.pinimg.com/originals/a2/8c/c9/a28cc94b51b04802f7d033e7c206cbb7.jpg')
-stinky.photos.attach(io: tiger_img, filename: 'tiger.jpg', content_type: 'image/jpg')
+tiger_img = URI.open('https://i.pinimg.com/originals/a2/8c/c9/a28cc94b51b04802f7d033e7c206cbb7.jpg') if loadImages
+stinky.photos.attach(io: tiger_img, filename: 'tiger.jpg', content_type: 'image/jpg') if loadImages
 
 iguana = Pet.new(name: 'Iguani', price_in_eur: 15, pet_type: 'Iguana', description: 'My favorite iguana')
-iguana_img = URI.open('https://www.thoughtco.com/thmb/V_hCTwhYWNsCogFPnNHsedAEsTA=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/iguana2-b554e81fc1834989a715b69d1eb18695.jpg')
-iguana.photos.attach(io: iguana_img, filename: 'iguana.jpg', content_type: 'image/jpg')
+iguana_img = URI.open('https://www.thoughtco.com/thmb/V_hCTwhYWNsCogFPnNHsedAEsTA=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/iguana2-b554e81fc1834989a715b69d1eb18695.jpg') if loadImages
+iguana.photos.attach(io: iguana_img, filename: 'iguana.jpg', content_type: 'image/jpg') if loadImages
 
 blubby = Pet.new(name: 'Blubby', price_in_eur: 34.3, pet_type: 'Fish', description: 'Blubby is a great fish')
-fish_img = URI.open('https://news.cnrs.fr/sites/default/files/styles/visuel_principal/public/assets/images/nemo2sig_72dpi.jpg')
-blubby.photos.attach(io: fish_img, filename: 'fish.jpg', content_type: 'image/jpg')
+fish_img = URI.open('https://news.cnrs.fr/sites/default/files/styles/visuel_principal/public/assets/images/nemo2sig_72dpi.jpg') if loadImages
+blubby.photos.attach(io: fish_img, filename: 'fish.jpg', content_type: 'image/jpg') if loadImages
 
 mauwie = Pet.new(name: 'Mauwie', price_in_eur: 3.55, pet_type: 'Cat', description: 'My cat mauwie is so awesome')
-cat_img = URI.open('https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80')
-mauwie.photos.attach(io: cat_img, filename: 'cat.jpg', content_type: 'image/jpg')
+cat_img = URI.open('https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80') if loadImages
+mauwie.photos.attach(io: cat_img, filename: 'cat.jpg', content_type: 'image/jpg') if loadImages
 
 perry = Pet.new(name: 'Perry', price_in_eur: 300, pet_type: 'Parrot', description: 'My parrot perry is so awesome')
-parrot_img = URI.open('https://images.unsplash.com/photo-1544181093-c712fb401bdc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60')
-perry.photos.attach(io: parrot_img, filename: 'parrot.jpg', content_type: 'image/jpg')
+parrot_img = URI.open('https://images.unsplash.com/photo-1544181093-c712fb401bdc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60') if loadImages
+perry.photos.attach(io: parrot_img, filename: 'parrot.jpg', content_type: 'image/jpg') if loadImages
 
 boris = User.create(username: 'Boris', password: 'Password', password_confirmation: 'Password', email: 'boris@example.com')
 svenia = User.create(username: 'Svenia', password: 'Password', password_confirmation: 'Password', email: 'svenia@example.com')
 
-avatar_boris = URI.open('https://avatars1.githubusercontent.com/u/2471555?s=460&v=4')
-avatar_svenia = URI.open('https://heroes.ch/wp-content/uploads/2019/10/svenia-wurm-1-600x400.jpg')
+# add a geolocation to each user
+parisLoc = Geolocation.create(latitude: 48.849496, longtitude: 2.337695)
+amsterdamLoc = Geolocation.create(latitude: 52.373700, longtitude: 4.890397)
 
-boris.avatar.attach(io: avatar_boris, filename: 'avatar.jpg', content_type: 'image/jpg')
-svenia.avatar.attach(io: avatar_svenia, filename: 'avatar.jpg', content_type: 'image/jpg')
+boris.geolocation = parisLoc
+svenia.geolocation = amsterdamLoc
+
+avatar_boris = URI.open('https://avatars1.githubusercontent.com/u/2471555?s=460&v=4') if loadImages
+avatar_svenia = URI.open('https://heroes.ch/wp-content/uploads/2019/10/svenia-wurm-1-600x400.jpg') if loadImages
+
+boris.avatar.attach(io: avatar_boris, filename: 'avatar.jpg', content_type: 'image/jpg') if loadImages
+svenia.avatar.attach(io: avatar_svenia, filename: 'avatar.jpg', content_type: 'image/jpg') if loadImages
 
 boris.save
 svenia.save
