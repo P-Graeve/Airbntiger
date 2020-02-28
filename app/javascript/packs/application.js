e29788d5dee09739b91f6adb30290ca2c77e9eed
svenia wurm <svwu@outlook.com>
2020-02-28 13:26:08 +0100

    started working on the date injection

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import { addFormSubmitListener } from '../signup/form_submit';
 import { initPhotosChange } from '../new_pet/new_pet';
 import { refreshImages } from '../edit_pet/edit_pet';
 import { initEventListenerOnLandingPage } from '../homepage/submit_event';
+import { injectInitialDatesInForm } from '../show_pet/getDates';
 
 if (window.location.pathname === '/users/sign_up') {
   initAvatarUploadBtn();
@@ -19,4 +20,6 @@ if (window.location.pathname.match(/edit/)) {
   refreshImages();
 }
 
+injectInitialDatesInForm();
 initEventListenerOnLandingPage();
+
