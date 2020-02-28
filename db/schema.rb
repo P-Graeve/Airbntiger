a2657fb8303c8dfae084213c0e40b1049f1ee4aa
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-02-28 12:49:42 +0100

    add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_125708) do
+ActiveRecord::Schema.define(version: 2020_02_28_111439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,17 @@ ActiveRecord::Schema.define(version: 2020_02_27_125708) do
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +97,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_125708) do
   add_foreign_key "bookings", "users"
   add_foreign_key "geolocations", "users"
   add_foreign_key "pets", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
